683f20e4a192b1c0670e4126ae18a8b3a8e2bddb
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-02-21 00:15:49 +0300

    error mesajlariveklendi

--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useForm } from "react-hook-form";
 
 export default function TaskHookForm({ kisiler, submitFn }) {
+
+  const {register, handleSubmit, formState: {errors}} = useForm({
+    defaultValues: {
+      title: "",
+      description: "",
+      people: []
+    }
+  });
+  console.log(errors)
+  const onSubmit = (data) => { console.log(data) };
+
   return (
-    <form className="taskForm">
+    <form className="taskForm" onSubmit={handleSubmit(onSubmit)}>
       <div className="form-line">
         <label className="input-label" htmlFor="title">
           Başlık
@@ -12,8 +24,15 @@ export default function TaskHookForm({ kisiler, submitFn }) {
           id="title"
           name="title"
           type="text"
+          {...register("title", {
+            required: "Task başlığı yazmalısınız",
+            minLength:{
+              value: 4,
+              message: "Task başlığı en az 3 karakter olmalı"
+            }
+            })}
         />
-        <p className="input-error">{"formErrors.title"}</p>
+        <p className="input-error">{errors.title?.message}</p>
       </div>
 
       <div className="form-line">
@@ -25,8 +44,15 @@ export default function TaskHookForm({ kisiler, submitFn }) {
           rows="3"
           id="description"
           name="description"
+          {...register("description", {
+            required: "Task açıklaması yazmalısınız",
+            minLength:{
+              value: 10,
+              message: "Task açıklaması en az 10 karakter olmalı"
+            }
+            })}
         ></textarea>
-        <p className="input-error">{"formErrors.description"}</p>
+        <p className="input-error">{errors.description?.message}</p>
       </div>
 
       <div className="form-line">
@@ -38,12 +64,22 @@ export default function TaskHookForm({ kisiler, submitFn }) {
                 type="checkbox"
                 name="people"
                 value={p}
+                {...register("people", {
+                  minLength:{
+                    value: 1,
+                    message: "Lütfen en az bir kişi seçin"
+                  },
+                  maxLength:{
+                    value: 3,
+                    message: "En fazla 3 kişi seçebilirsiniz"
+                  }
+                  })}
               />
               {p}
             </label>
           ))}
         </div>
-        <p className="input-error">{""}</p>
+        <p className="input-error">{errors.people?.message}</p>
       </div>
 
       <div className="form-line">
